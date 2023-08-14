1c1a2ecb2465cfb0cfaf1d3a268ea7ac265e9449
Ricardoonz <98755098+Ricardoonz@users.noreply.github.com>
2023-08-14 10:50:51 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 import random
 
-# This txt file
 with open("words.txt") as file:
     word_list = [line.strip() for line in file]
 
-# Select a random word from the list
 word = random.choice(word_list)
-
-# Create a set of the letters in the word
 word_letters = set(word)
-
-# Create an empty set to store the player's guesses
 player_guesses = set()
-
-# Set the number of mistakes allowed
 max_mistakes = 7
 
-# Hangman ASCII art (same as before)
 HANGMAN = (
     """
     -----
@@ -104,9 +95,7 @@ HANGMAN = (
     """,
 )
 
-# Loop until the game is over
 while True:
-    # Print the current state of the word with underscores for unguessed letters
     print("Word: ", end="")
     for letter in word:
         if letter in player_guesses:
@@ -115,26 +104,17 @@ while True:
             print("_", end="")
     print()
 
-    # Draw the hangman
     print(HANGMAN[max_mistakes])
-
-    # Display the number of guesses remaining
     print("Guesses remaining:", max_mistakes)
-
-    # Ask the player to guess a letter
     guess = input("Guess a letter: ")
-
-    # Add the guess to the player's set of guesses
     player_guesses.add(guess)
 
-    # Check if the guess is correct
     if guess in word_letters:
         print("Correct!")
     else:
         print("Incorrect!")
         max_mistakes -= 1
 
-    # Check if the game is over
     if max_mistakes == 0:
         print("You lost! The word was", word)
         break
